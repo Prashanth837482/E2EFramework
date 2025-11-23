65ac574ecbeced3757cf34805b02f9902b95624f
Prashanth <prashanthvangapelli369@gmail.com>
2025-11-23 19:02:27 +0530

    update config file path logic

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -9,8 +10,10 @@ public class ConfigReader {
 
     public static void loadConfig() {
         try {
-        	
-            FileInputStream fis = new FileInputStream("C:\\AutomationJava\\workspace\\RestAssured\\E2EFramework\\config\\configuration.properties");
+        	String projectPath = System.getProperty("user.dir");
+        	//https://chatgpt.com/c/69230a1a-70c0-8320-b750-3f1f1265ecb7   -> link for file path generation and reason for file.separator usage
+            //FileInputStream fis = new FileInputStream(projectPath+"/config/configuration.properties");
+        	FileInputStream fis = new FileInputStream(projectPath + File.separator + "config"+ File.separator +"configuration.properties");
             prop = new Properties();
             prop.load(fis);
         } catch (Exception e) {
